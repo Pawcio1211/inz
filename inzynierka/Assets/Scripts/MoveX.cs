731644b266bebf6c683ed118fd810d15eb674c42
Pawcio1211 <65865764+Pawcio1211@users.noreply.github.com>
2022-12-05 16:47:24 +0100

    Na nowo dodanie ruchu piłki w osi X

---
+++ inzynierka/Assets/Scripts/MoveX.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveX : MonoBehaviour
+{
+    public float speed;
+    public Vector3 startposition;
+    public Transform centre;
+    private float posX, posZ, angle = 0;
+    float r = 7;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = startposition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angle = angle + Time.deltaTime * speed;
+
+        posX = centre.position.x + Mathf.Sin(angle) * r;
+        posZ = centre.position.x + Mathf.Cos(angle) * r;
+        transform.position = new Vector3(posX, 0, posZ);
+
+        if(angle >= 2f || angle <= -2f)
+        {
+            speed = -speed;
+        }
+
+        if(angle > 360)
+        {
+            angle = 0;
+        }
+    }
+}
