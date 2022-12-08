2543b4e5bed5bba834256972eef52a68585d82b5
Alex10080 <98435993+Alex10080@users.noreply.github.com>
2022-12-08 13:05:20 +0100

    menu3D
    Na razie tylko przycisk górny, bez napisów

---
+++ inzynierka/Assets/Scripts/menu_kolorki.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu_kolorki : MonoBehaviour
+{
+    public void Red()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+}
