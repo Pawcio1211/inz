86a831351aa9e306b5c3c1801f6419872e5d33ae
AJBywalska <65554965+AJBywalska@users.noreply.github.com>
2022-12-18 23:48:04 +0100

    Trening osi Z
    Nie mam pojęcia co się tam odpierdziela

---
+++ inzynierka/Assets/Scripts/MoveZ.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveZ : MonoBehaviour
+{
+    public float speed;
+    public Vector3 startposition;
+    public Transform centre;
+    private float posX, posZ, angle = 0;
+    float r = 7;
+    bool start = false, side;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = startposition;
+        side = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angle = angle + Time.deltaTime * speed;
+        transform.Rotate(new Vector3(0, 0, angle) * speed);
+
+        Debug.Log(angle);
+    }
+
+    public void Set()
+    {
+        if (angle >= 1.0f)
+        {
+            side = true;
+        }
+        if (side == false)
+        {
+            speed = -speed;
+        }
+
+        if (angle <= -1.0f)
+        {
+            side = false;
+        }
+        if (side == true)
+        {
+            speed = -speed;
+        }
+    }
+    public void Remove() { transform.Rotate(new Vector3(0, 0, 0) * speed); }
+}
